0ef5e840dd4e7404eddf106a1eae7a9dc1e38c8e
Alexander Huba <alexandervhuba@gmail.com>
2020-06-20 11:10:56 +0300

    Add lombok to common module

--- messaging-common/src/main/java/ua/ahuba/messaging/Order.java
+++ messaging-common/src/main/java/ua/ahuba/messaging/Order.java
@@ -1,7 +1,9 @@
 package ua.ahuba.messaging;
 
+import lombok.Data;
 import java.util.List;
 
+@Data
 public class Order {
     private Long id;
     private OrderStatus status;
@@ -9,52 +11,4 @@ public class Order {
     private Long customerId;
     private Long accountId;
     private List<Long> productIds;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public OrderStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public Long getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(Long customerId) {
-        this.customerId = customerId;
-    }
-
-    public Long getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(Long accountId) {
-        this.accountId = accountId;
-    }
-
-    public List<Long> getProductIds() {
-        return productIds;
-    }
-
-    public void setProductIds(List<Long> productIds) {
-        this.productIds = productIds;
-    }
 }
