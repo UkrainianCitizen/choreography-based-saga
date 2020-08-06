a081944813e00316161a7652099099ed1202e8db
Alexander Huba <alexandervhuba@gmail.com>
2020-08-06 22:08:58 +0300

    Optimize imports

--- order-service/src/main/java/ua/citizen/messagedrivenmicroservices/orderservice/controller/OrderController.java
+++ order-service/src/main/java/ua/citizen/messagedrivenmicroservices/orderservice/controller/OrderController.java
@@ -3,21 +3,14 @@ package ua.citizen.messagedrivenmicroservices.orderservice.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
-import org.apache.tomcat.jni.OS;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.EmitterProcessor;
-import reactor.core.publisher.Flux;
 import ua.citizen.messagedrivenmicroservices.messaging.Order;
-import ua.citizen.messagedrivenmicroservices.orderservice.repository.OrderRepository;
 import ua.citizen.messagedrivenmicroservices.orderservice.service.OrderService;
-import java.util.function.Supplier;
 
 @RestController
 @Slf4j
