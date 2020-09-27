90614433792c23d5c4062a307adc4b87cc67f8cd
Alexander Huba <alexandervhuba@gmail.com>
2020-09-27 20:20:38 +0300

    Add a messaging test for account app

--- account-service/src/test/java/ua/citizen/messagedrivenmicroservices/accountservice/controller/AccountControllerTest.java
+++ account-service/src/test/java/ua/citizen/messagedrivenmicroservices/accountservice/controller/AccountControllerTest.java
@@ -1,49 +1,58 @@
 package ua.citizen.messagedrivenmicroservices.accountservice.controller;
 
-import org.junit.runner.RunWith;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.cloud.stream.binder.test.InputDestination;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+import ua.citizen.messagedrivenmicroservices.accountservice.AccountApplication;
+import ua.citizen.messagedrivenmicroservices.messaging.Order;
+import ua.citizen.messagedrivenmicroservices.messaging.OrderStatus;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = {AccountApplication.class})
+@Import(TestChannelBinderConfiguration.class)
 public class AccountControllerTest {
-//
-//    private static final Logger LOGGER = LoggerFactory.getLogger(AccountControllerTest.class);
-//
-//    @Autowired
-//    private Processor processor;
-//    @Autowired
-//    private MessageCollector messageCollector;
-//
-//    @Test
-//    @SuppressWarnings("unchecked")
-//    public void testAccepted() {
-//        Order o = new Order();
-//        o.setId(1L);
-//        o.setAccountId(1L);
-//        o.setCustomerId(1L);
-//        o.setPrice(500);
-//        o.setProductIds(Collections.singletonList(2L));
-//        processor.input().send(MessageBuilder.withPayload(o).build());
-//        Message<Order> received = (Message<Order>) messageCollector.forChannel(processor.output()).poll();
-//        LOGGER.info("Order response received: {}", received.getPayload());
-//        assertNotNull(received.getPayload());
-//        assertEquals(OrderStatus.ACCEPTED, received.getPayload().getStatus());
-//    }
-//
-//    @Test
-//    @SuppressWarnings("unchecked")
-//    public void testRejected() {
-//        Order o = new Order();
-//        o.setId(1L);
-//        o.setAccountId(1L);
-//        o.setCustomerId(1L);
-//        o.setPrice(100000);
-//        o.setProductIds(Collections.singletonList(2L));
-//        processor.input().send(MessageBuilder.withPayload(o).build());
-//        Message<Order> received = (Message<Order>) messageCollector.forChannel(processor.output()).poll();
-//        LOGGER.info("Order response received: {}", received.getPayload());
-//        assertNotNull(received.getPayload());
-//        assertEquals(OrderStatus.REJECTED, received.getPayload().getStatus());
-//    }
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private InputDestination source;
+
+    @Autowired
+    private OutputDestination target;
+
+    @Test
+    public void functionTest() throws IOException {
+
+        source.send(new GenericMessage<>(prepareInputOrder()));
+
+        Message<byte[]> received = target.receive();
+
+        assertNotNull(received.getPayload());
+        assertTrue(isOutputCorrect(received));
+    }
+
+    private boolean isOutputCorrect(Message<byte[]> received) throws IOException {
+        Order result = (objectMapper.readValue(received.getPayload(), Order.class));
+
+        return result.getStatus().equals(OrderStatus.ACCEPTED);
+    }
+
+    private Order prepareInputOrder() {
+        Order order = new Order();
+        order.setStatus(OrderStatus.NEW);
+        order.setCustomerId(1L);
+        return order;
+    }
 }
